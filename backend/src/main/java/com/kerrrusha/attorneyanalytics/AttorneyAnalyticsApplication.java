683a189582b639b1b7810adad29aafb44e4e8193
Kirill Koval <brcyrill2@gmail.com>
2024-03-11 20:42:10 +0200

    init commit

---
+++ backend/src/main/java/com/kerrrusha/attorneyanalytics/AttorneyAnalyticsApplication.java
@@ -0,0 +1,11 @@
+package com.kerrrusha.attorneyanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttorneyAnalyticsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AttorneyAnalyticsApplication.class, args);
+    }
+}
