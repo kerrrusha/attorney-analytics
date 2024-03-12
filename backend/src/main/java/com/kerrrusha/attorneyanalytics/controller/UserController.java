ffd2ce3b326a400e52e9be9a618512ca2d7e23f3
Kirill Koval <brcyrill2@gmail.com>
2024-03-12 14:58:01 +0200

    added user controller

---
+++ backend/src/main/java/com/kerrrusha/attorneyanalytics/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.kerrrusha.attorneyanalytics.controller;
+
+import com.kerrrusha.attorneyanalytics.dto.user.response.UserResponseDto;
+import com.kerrrusha.attorneyanalytics.model.User;
+import com.kerrrusha.attorneyanalytics.repository.UserRepository;
+import com.kerrrusha.attorneyanalytics.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+    private final UserRepository userRepository;
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/info")
+    public UserResponseDto getUserInfo(Principal principal) {
+        String email = principal.getName();
+        return userService.findByEmail(email);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/authorized")
+    public void isAuthorized() {}
+}
--- backend/src/main/java/com/kerrrusha/attorneyanalytics/dto/user/request/UserUpdateRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.kerrrusha.attorneyanalytics.dto.user.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record UserUpdateRequestDto(
-        @NotBlank String userId,
-        String firstName,
-        String lastName
-) {}
