cdd239e432eeb836a0321ebb0a98002f1bce506a
Kirill Koval <brcyrill2@gmail.com>
2024-03-14 00:46:03 +0200

    removed redundant field

--- backend/src/main/java/com/kerrrusha/attorneyanalytics/model/user/Role.java
+++ backend/src/main/java/com/kerrrusha/attorneyanalytics/model/user/Role.java
@@ -4,12 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -35,10 +32,6 @@ public class Role {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, unique = true)
     private RoleName name;
-
-    @JoinColumn(name = "user_id")
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User user;
 
     @Column(nullable = false)
     private boolean deleted = false;
