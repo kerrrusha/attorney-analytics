9f3ff2d50c361bc09f9aa4094343b806442e9cd4
Kirill Koval <brcyrill2@gmail.com>
2024-03-16 01:33:00 +0200

    added many to many fields

--- backend/src/main/java/com/kerrrusha/attorneyanalytics/model/case_/Case.java
+++ backend/src/main/java/com/kerrrusha/attorneyanalytics/model/case_/Case.java
@@ -1,13 +1,21 @@
 package com.kerrrusha.attorneyanalytics.model.case_;
 
+import com.kerrrusha.attorneyanalytics.model.client.Client;
 import com.kerrrusha.attorneyanalytics.model.user.Title;
+import com.kerrrusha.attorneyanalytics.model.user.User;
+import com.kerrrusha.attorneyanalytics.model.user.UserPracticeArea;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,6 +27,8 @@ import org.hibernate.annotations.SQLRestriction;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -42,6 +52,24 @@ public class Case {
     private String title;
 
     private String description;
+
+    @ToString.Exclude
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "case_user",
+            joinColumns = @JoinColumn(name = "case_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> assignedUsers = new ArrayList<>();
+
+    @ToString.Exclude
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "case_client",
+            joinColumns = @JoinColumn(name = "case_id"),
+            inverseJoinColumns = @JoinColumn(name = "client_id")
+    )
+    private List<Client> assignedClients = new ArrayList<>();
 
     @ManyToOne
     @JoinColumn(name = "case_status_id")
