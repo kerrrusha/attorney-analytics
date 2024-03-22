56d6c8e94f9c82437b8dd192f742c6cac06a4dd5
Kirill Koval <brcyrill2@gmail.com>
2024-03-22 04:25:40 +0200

    fixed

--- frontend/src/components/IdValuesSelect.tsx
+++ frontend/src/components/IdValuesSelect.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import {IdValuePair} from "../common/commonTypes";
+import {useTranslation} from "react-i18next";
 
 interface IdValuePairsSelect {
     name: string,
@@ -8,7 +9,8 @@ interface IdValuePairsSelect {
 }
 
 export default function IdValuePairsSelect({name, options, handleSetId}: IdValuePairsSelect) {
-    const [id, setId_] = useState<number>(0);
+    const [id, setId_] = useState<number>(-1);
+    const { t } = useTranslation();
 
     const setTitleId = (titleId: number) => {
         setId_(titleId);
@@ -22,7 +24,7 @@ export default function IdValuePairsSelect({name, options, handleSetId}: IdValue
                 className="text-black border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
                 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
                 dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option value="" selected hidden>Choose {name}</option>
+            <option value={-1} hidden>{t("common.choose")} {name}</option>
             {options.map((col, index) => <option key={index} value={col.id}>{col.value}</option>)}
         </select>
     </>);
